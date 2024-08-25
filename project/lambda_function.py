0034a5c4cc0a633b578cef93338d4bc8611ae6f5
sanjanak98@gmail.com <sanjanak98@gmail.com>
2024-08-25 15:46:23 -0400

    download mdoel at runtime

--- project/lambda_function.py
+++ project/lambda_function.py
@@ -2,8 +2,12 @@ import json
 from hydra import compose, initialize
 
 from prediction_service.inference import Inference
+from project.load_model import load_model_uri_from_config, download_model
 
 def lambda_handler(event, context):
+	model_uri = load_model_uri_from_config()
+	download_model(model_uri)
+
 	with initialize(config_path="configs"):
 		cfg = compose(config_name="config")
 
@@ -24,9 +28,9 @@ def lambda_handler(event, context):
 		"body": json.dumps(response.cpu().numpy().tolist())
 	}
 
-if __name__ == "__main__":
-	event = {
-		"sentence": "This is a test sentence."
-	}
-	resp = lambda_handler(event, None)
-	print(resp)
+# if __name__ == "__main__":
+# 	event = {
+# 		"sentence": "This is a test sentence."
+# 	}
+# 	resp = lambda_handler(event, None)
+# 	print(resp)
--- project/load_model.py
+++ project/load_model.py
@@ -19,6 +19,6 @@ def download_model(model_uri):
     # torch.save(model.state_dict(), os.path.join(os.getcwd(),  "models/model.pth"))
     torch.save(model.state_dict(), os.path.join("/tmp/transformers_cache", "model.pth"))
 
-if __name__ == "__main__":
-    model_uri = load_model_uri_from_config()
-    download_model(model_uri)
+# if __name__ == "__main__":
+#     model_uri = load_model_uri_from_config()
+#     download_model(model_uri)
