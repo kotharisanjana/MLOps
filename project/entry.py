402d59cd1cf55dd3dd13779e24345c90498b334a
sanjanak98@gmail.com <sanjanak98@gmail.com>
2024-09-28 16:48:12 -0400

    server deployment

--- project/entry.py
+++ project/entry.py
@@ -1,15 +1,17 @@
 import random
-from prometheus_client import start_http_server, Gauge
+from prometheus_client import start_http_server, Gauge, Counter
 from hydra import compose, initialize
 import threading
 import time
+import sys
 
 from prediction_service.inference import Inference
 
 ACCURACY = Gauge("prediction_accuracy", "Accuracy of the model")
+INVOCATIONS = Counter("invocation_count", "Number of invocations")
 
 def start_server():
-    start_http_server(port=5003, addr="0.0.0.0")
+    start_http_server(port=5001, addr="0.0.0.0")
     while True:
 	    time.sleep(1)
     
@@ -23,11 +25,12 @@ def inference(event):
 	print(response, flush=True)
 	
 	ACCURACY.set(random.random())
+	INVOCATIONS.inc()
 
 
 if __name__ == "__main__":
-	server_thread = threading.Thread(target=start_server)  
-	server_thread.daemon = True    
+	server_thread = threading.Thread(target=start_server) 
+	server_thread.daemon = True 
 	server_thread.start()
 
 	try:
@@ -37,6 +40,10 @@ if __name__ == "__main__":
 			}
 			
 			inference(event)
-			time.sleep(5) 
+			time.sleep(20) 
 	except KeyboardInterrupt:
 		print("Shutting down")
+		sys.exit(0)
+	except Exception as e:  
+		print(f"Unexpected error: {e}")
+		sys.exit(1)
